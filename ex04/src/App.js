f869b1318305de9769ccf2befd21a487e460f3a2
badnc <b.dunic@hotmail.com>
2021-04-02 19:23:36 +0200

    React

---
+++ ex04/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import GetRequestHeaders from './GetRequestSetHeaders';
+
+function App() {
+  return (
+    <div className="App">
+      <GetRequestHeaders />
+    </div>
+  );
+}
+
+export default App;
